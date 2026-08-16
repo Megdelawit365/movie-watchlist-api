f4aef86b5b1e468c131741947026c335140b1011
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-08-16 21:13:13 +0300

    added middlewares

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
+import notFound from "./middlewares/notFound.js"
 import { express } from express
 import { movieRoutes } from "./routes/movieRoutes.js"
-const app = express()
-app.use(express.json())
+import { requestLogger } from "./middlewares/requestLogger.js"
 
+const app = express()
+
+app.use(express.json())
+app.use(requestLogger)
 app.use("/api/movies", movieRoutes)
+app.use(notFound)
 
 export default app
