df1ded1d09b13975776ff081d6939c1a4ce03e92
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-08-16 19:33:12 +0300

    added middleware for validating movie updates

---
+++ src/middlewares/validateMovieUpdate.js
@@ -0,0 +1,38 @@
+const validateUpdate = (req, res, next) => {
+    const title = req.body.title
+    const id = req.body.id
+    const genre = req.body.genre
+    const watched = req.body.watched
+    const rating = req.body.rating
+
+    if (title != undefined && (title.trim() === "" || typeof (title) != "string")) {
+        return res.status(400).json({
+            message: "Invalid title."
+        })
+    }
+    if (id != undefined) {
+        return res.status(400).json({
+            message: "Id cannot be updated."
+        })
+    }
+    if (genre != undefined && (genre.trim() === "" || typeof (genre) != "string")) {
+        return res.status(400).json({
+            message: "Invalid genre."
+        })
+    }
+    if (watched != undefined && typeof (watched) != "boolean") {
+        return res.status(400).json({
+            message: "Invalid watched status."
+        })
+    }
+    if (rating != undefined && (typeof (rating) != "number" || rating < 1 || rating > 5)) {
+        return res.status(400).json({
+            message: "Invalid rating."
+        })
+    }
+
+    next()
+
+}
+
+export default validateUpdate
