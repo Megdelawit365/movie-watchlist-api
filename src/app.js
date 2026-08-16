d8bb91dc5d34ba49e2452256c94448d1b8ca4741
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-08-16 20:14:22 +0300

    added movie routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import { express } from express
-
+import { movieRoutes } from "./routes/movieRoutes.js"
 const app = express()
 app.use(express.json())
+
+app.use("/api/movies", movieRoutes)
 
 export default app
