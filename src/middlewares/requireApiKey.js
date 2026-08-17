ffcf3cd0b910942629a43f527e50ae0bbbdb6a90
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-08-17 18:25:54 +0300

    added middleware for api key

---
+++ src/middlewares/requireApiKey.js
@@ -0,0 +1,10 @@
+const requireApiKey = (req, res, next) => {
+    const apiKey = req.headers["x-api-key"]
+    if (!apiKey || apiKey != "movie-class-2026") {
+        return res.status(403).json({
+            message: "Api key is missing or incorrect."
+        })
+    }
+}
+
+export default requireApiKey
